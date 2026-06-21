f7ae239f74ab44e1f0a24ab7059a26ae9cd679d6
Aintdoe99 <aintdoe@gmx.de>
2026-06-21 16:19:02 +0200

    Upgrade service worker cache version and assets

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,33 @@
-const CACHE_NAME = 'cashback-tracker-v1';
+const CACHE_NAME = 'cashback-tracker-v2';
+
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/app-icon.png',
-  './icons/android-chrome-192.png',
-  './icons/android-chrome-512.png',
-  './icons/android-chrome-maskable-512.png',
-  './icons/apple-touch-icon.png'
+  './icons/icon-192.svg',
+  './icons/icon-512.svg'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); }))).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.map(key => key !== CACHE_NAME ? caches.delete(key) : undefined)
+      ))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
